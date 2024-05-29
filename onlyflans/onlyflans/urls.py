522f422b9e3df66547671e9487b6b7995222c9ce
Leonardo Llaupe <leonardollaupe@gmail.com>
2024-05-29 09:58:18 -0400

    se añade screenshot req hito1 y comienzo de req. hito2

--- onlyflans/onlyflans/urls.py
+++ onlyflans/onlyflans/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from web.views import index, about, welcome
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', index, name="index"),
+    path('acerca/', about, name="about"),
+    path('bienvenido/', welcome, name="welcome"), 
 ]
